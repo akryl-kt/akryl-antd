a93479ba7cc7ffb14798750ef322a53cd7b3165c
Александр Шилов <aleksandr.schilow2012@gmail.com>
2020-05-11 20:58:27 +0300

    Add Checkbox.Group

--- src/main/kotlin/io/akryl/antd/Checkbox.kt
+++ src/main/kotlin/io/akryl/antd/Checkbox.kt
@@ -11,9 +11,19 @@ import react.React
 import react.ReactElement
 import kotlin.js.json
 
-@JsModule("antd/lib/checkbox")
+@JsModule("antd/lib/checkbox/Checkbox")
 @JsNonModule
 private external val Checkbox: AntdComponent
+
+@JsModule("antd/lib/checkbox/Group")
+@JsNonModule
+private external val Group: AntdComponent
+
+data class CheckboxOptions<T>(
+    val label: ReactElement<*>,
+    val value: T,
+    val disabled: Boolean? = undefined
+)
 
 object checkbox {
     operator fun invoke(
@@ -42,5 +52,30 @@ object checkbox {
             "style" to style?.toStyleJson()
         ),
         children = *concatChildrenToArray(children, child, text)
+    )
+
+    fun <T> group(
+        disabled: Boolean? = undefined,
+        name: String? = undefined,
+        options: List<CheckboxOptions<T>>? = undefined,
+        onChange: ((values: List<T>) -> Unit)? = undefined,
+
+        css: CssStyle? = undefined,
+        className: CharSequence? = undefined,
+        style: CssStyle? = undefined
+    ) = React.createElement(
+        Group.default,
+        json(
+            "disabled" to disabled,
+            "name" to name,
+            "options" to options?.toTypedArray().orUndefined(),
+            "onChange" to onChange?.let { fn ->
+                { values: Array<T> ->
+                    fn(values.asList())
+                }
+            }.orUndefined(),
+            "className" to concatStyle(css, className),
+            "style" to style?.toStyleJson()
+        )
     )
 }
